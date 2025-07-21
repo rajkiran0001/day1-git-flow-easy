a31305f7ac75dc88117390aebf47ff930550a971
rajkiran <rajkiran.one@gmail.com>
2025-07-21 18:10:31 +0200

    add toggle theme feature

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("JS is connected! 🚀");
+const toggleButton = document.getElementById('theme-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  toggleButton.textContent = document.body.classList.contains('dark-mode') ? 'Light Theme' : 'Dark Theme';
+});
